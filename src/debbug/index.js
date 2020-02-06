c2570cc279d53ac9b5b0ad7802d8440dc5b6a2c7
Alexis Ramos <alexisramoscaballero@gmail.com>
2020-02-06 13:41:09 -0600

    update debbug index

--- src/debbug/index.js
+++ src/debbug/index.js
@@ -11,3 +11,21 @@ function hello () {
 
 
  hello();
+
+
+
+
+ const moneyBox = () => {
+    var saveCoins =0;
+    const countCoins = (coins) => {
+        saveCoins += coins;
+        console.log(`MoneBox: $${saveCoins}`);
+    };
+    return countCoins;
+};
+
+let myMoneyBox = moneyBox()
+
+myMoneyBox(4);
+myMoneyBox(6);
+myMoneyBox(10);
